e63fa251f58a1180326df57cfcc93b2c7ae63881
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-02 00:19:54 +0100

    updated the gray taliwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'gray': {
+        50: '#efefef',
+        100: '#d5d5d5',
+        200: '#adadad',
+        300: '#979797',
+        400: '#747474',
+        500: '#5c5c5c',
+        600: '#494949',
+        700: '#373737',
+        800: '#252424',
+        900: '#181818',
+      },
+    }
   },
   plugins: [],
 }

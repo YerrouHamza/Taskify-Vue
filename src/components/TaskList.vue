2cdd2034f6633ba4fead2b83571a29ac766292a4
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-03 02:23:18 +0100

    small update

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -2,9 +2,9 @@
     <section class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4">
         <TaskCard v-for="task in tasks" :key="task.id" :title="task.title" :text="task.text" :statue="task.statue" :id="task.id"/>
         <AddTaskCard />
-        <AddModal />
-        <EditModal />
     </section>
+    <AddModal />
+    <EditModal />
 </template>
 
 <script setup>

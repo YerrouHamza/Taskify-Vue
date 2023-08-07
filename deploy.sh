72c16fe476ca9f180263cbe366e2ebc1754942bb
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-07 18:34:32 +0100

    vue deploy files update

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YerrouHamza/task-manager-vue.git main:gh-pages
+git push -f git@github.com:YerrouHamza/task-manager-vue.git master:gh-pages
 
 cd -
